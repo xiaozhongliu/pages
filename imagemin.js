7e30224bf8d738f79d9886afe1ba23200cf364ae
XiaozhongLiu <xiaozhong.liu@outlook.com>
2017-12-09 13:45:30 +0800

    minify images

---
+++ imagemin.js
@@ -0,0 +1,13 @@
+const imagemin = require('imagemin')
+const imageminJpegtran = require('imagemin-jpegtran')
+const imageminPngquant = require('imagemin-pngquant')
+
+imagemin(['assets/posts/20171209/*.{jpg,png}'], 'dist', {
+	plugins: [
+		imageminJpegtran(),
+		imageminPngquant({quality: '65-75'})
+	]
+}).then(files => {
+	console.log(files)
+	//=> [{data: <Buffer 89 50 4e …>, path: 'build/images/foo.jpg'}, …]
+})
